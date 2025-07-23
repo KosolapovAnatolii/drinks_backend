f8d8ec7c6dc6867f01774b24ae35112a39296182
Анатолий Косолапов <anatolii@MacBook-Air-Anatolij.local>
2025-07-23 17:41:43 +0300

    update port

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ app.use('/api/auth', authRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(5000, () => console.log('Server running on http://localhost:5000'));
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server running on http://localhost:${process.env.PORT || 5000}`);
+    });
   })
   .catch(err => console.error(err));
