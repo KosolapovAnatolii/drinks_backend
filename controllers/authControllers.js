3ed492e437bfa4edfc0e4ac6877429946ade9671
Анатолий Косолапов <anatolii@MacBook-Air-Anatolij.local>
2025-07-23 17:32:00 +0300

    made auth endpoints

---
+++ controllers/authControllers.js
@@ -0,0 +1,42 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+exports.register = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ email, password: hashedPassword });
+    res.status(201).json({ message: 'User created', userId: user._id });
+  } catch (err) {
+    res.status(400).json({ error: 'User already exists' });
+  }
+};
+
+exports.test = async (req, res) => {
+  const { msg } = req.body;
+  try {
+    console.log(msg);
+    res.status(201).json({ message: 'Your message recieved' });
+  } catch (err) {
+    res.status(400).json({ error: 'Error with message from. frontend' });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ error: 'Invalid credentials' });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ error: 'Invalid credentials' });
+
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: '7d' });
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ error: 'Server error' });
+  }
+};
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/authRoutes');
+
+dotenv.config();
+const app = express();
+
+const corsOptions = {
+  origin: ['http://localhost:9000', 'https://drinks-sand.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(5000, () => console.log('Server running on http://localhost:5000'));
+  })
+  .catch(err => console.error(err));
