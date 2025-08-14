70c70f6ca25f84f3766193899b8a06499eaa7d0b
Анатолий Косолапов <anatolii@MacBook-Air-Anatolij.local>
2025-08-14 18:34:21 +0300

    add delete route

--- routes/drinksRoutes.js
+++ routes/drinksRoutes.js
@@ -6,5 +6,6 @@ const auth = require('../middleware/auth');
 router.post('/', auth, drinksController.addDrink);
 router.get('/', auth, drinksController.getDrinks);
 router.get('/:slug', auth, drinksController.getDrink);
+router.delete('/:slug', auth, drinksController.deleteDrink)
 
 module.exports = router;
